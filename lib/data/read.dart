61af8de39337f263bb54936f2286bc54e941dee4
Bibek Bajagain <bibekbajagain074@gmail.com>
2023-05-19 20:05:22 +0545

    insert

---
+++ lib/data/read.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReadData extends StatefulWidget {
+  const ReadData({super.key});
+
+  @override
+  State<ReadData> createState() => _ReadDataState();
+}
+
+class _ReadDataState extends State<ReadData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
